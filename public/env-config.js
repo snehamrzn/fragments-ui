254e86d7c118503e2736e2e4864c48de9e913fe8
snehamrzn <smaharjan19@myseneca.ca>
2025-12-06 17:13:10 -0500

    add url

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,4 @@
 window.__ENV = {
-  API_ORIGIN: 'http://fragments-lb-394473185.us-east-1.elb.amazonaws.com',
-  // API_ORIGIN: 'http://localhost:8080',
+  // API_ORIGIN: 'http://fragments-lb-394473185.us-east-1.elb.amazonaws.com',
+  API_ORIGIN: 'https://fragments.smaharjan19.mystudentproject.ca',
 };
